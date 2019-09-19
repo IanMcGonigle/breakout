bc0d7c79665e4040dfbb314c7ddd9d134088ee7e
Ian McGonigle <reverend.magoo@gmail.com>
2019-09-19 10:07:48 -0400

    Adding game board

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
+/* eslint-disable */
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Game from "./views/Game.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
       component: Home
@@ -14,11 +16,21 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: Game
     }
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
+    // }
   ]
 });
---
+++ src/views/Game.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+  div
+    .container
+      .board
+        .field
+          .brick(v-for="brick in bricks")
+        .ball
+        .paddle
+</template>
+
+<script>
+export default {
+  name: "Game",
+  data() {
+    return {
+      bricks: []
+    };
+  },
+  created() {
+    this.bricks.length = 40;
+    alert("Created! ", this.bricks.length);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.board {
+  position: relative;
+  background: #000;
+  min-width: 300px;
+  min-height: calc(100vh - 300px);
+}
+
+.field {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.brick {
+  height: 30px;
+  margin: 2px;
+  background: #fff;
+  width: calc((100 / 8) * 1% - 4px);
+}
+
+.ball {
+  --size: 20px;
+  background: aquamarine;
+  width: var(--size);
+  height: var(--size);
+}
+
+.paddle {
+  height: 20px;
+  background: #fff;
+  width: calc((100 / 8) * 1% - 4px);
+  position: absolute;
+  bottom: 0;
+}
+</style>
